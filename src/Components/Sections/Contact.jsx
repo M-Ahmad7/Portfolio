20b4f8daf4e644e74a76f926543e958a6ea8c54e
M-Ahmad7 <m8876yz@gmail.com>
2025-08-05 11:24:14 -0700

    Deployed built site to root

--- src/Components/Sections/Contact.jsx
+++ src/Components/Sections/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     emialjs
       .sendForm(
-        import.meta.env.VITE_SERIVE_ID,
+        import.meta.env.VITE_SERVICE_ID,
         import.meta.env.VITE_TEMPLATE_ID,
         e.target,
         import.meta.env.VITE_PUBLIC_KEY
